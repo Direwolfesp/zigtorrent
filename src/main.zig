8530c9d809392ead19fc32b919b63ca9cc2bfd09
Direwolfesp <alvarolg365@gmail.com>
2025-04-15 02:16:46 +0200

    list parsing works, however i think the printing is bad

--- src/main.zig
+++ src/main.zig
@@ -13,25 +13,50 @@ const Value = union(enum) {
 
     pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
         switch (self) {
-            .string => |str| try writer.print("\"{s}\"\n", .{str}),
-            .integer => |int| try writer.print("{}\n", .{int}),
+            .string => |str| try writer.print("\"{s}\"", .{str}),
+            .integer => |int| try writer.print("{}", .{int}),
             .list => |list_opt| {
                 if (list_opt) |list| {
                     try writer.print("[", .{});
-                    for (list.items) |elem| try elem.format(fmt, options, writer);
+                    for (list.items, 0..) |elem, i| {
+                        try elem.format(fmt, options, writer);
+                        if (i < list.items.len - 1)
+                            try writer.print(",", .{});
+                    }
                     try writer.print("]", .{});
                 }
             },
         }
     }
 
-    pub fn deinit(self: *Value) void {
+    pub fn deinit(self: *@This()) void {
         switch (self.*) {
             .list => |*list_ptr| {
                 if (list_ptr.*) |*list| list.deinit();
             },
             else => {},
         }
+    }
+
+    // Returns how many bytes does the bencoded value occupies
+    // @example 12.len() == 4bytes == "i12e"
+    pub fn len(self: *const @This()) !usize {
+        return switch (self.*) {
+            .integer => |int| {
+                var digits: usize = 0;
+                var num: i64 = int;
+                while (num != 0) {
+                    digits += 1;
+                    num = @divFloor(num, 10);
+                }
+                const symbols: u8 = if (num >= 0) 2 else 3;
+                return digits + symbols;
+            },
+            .string => |str| {
+                return str.len + 1 + std.fmt.count("{}", .{str.len});
+            },
+            .list => error.Unimplemented,
+        };
     }
 };
 
@@ -50,10 +75,11 @@ pub fn main() !void {
         std.debug.print("Logs from your program will appear here\n", .{});
 
         const encodedStr = args[2];
-        const decodedStr = decodeBencode(encodedStr) catch {
+        var decodedStr = decodeBencode(encodedStr) catch {
             try stdout.print("Invalid encoded value\n", .{});
             std.process.exit(1);
         };
+        defer decodedStr.deinit();
         try stdout.print("{}", .{decodedStr});
     }
 }
@@ -82,29 +108,12 @@ fn decodeBencode(encodedValue: []const u8) !Value {
 
         var i: usize = 1;
         while (i < encodedValue.len) {
-            if (encodedValue[i] == 'e') {
+            if (encodedValue[i] == 'e')
                 break;
-            }
             const res = try decodeBencode(encodedValue[i..]);
             try decodedList.append(res);
-
-            switch (res) {
-                .integer => |int| {
-                    var digits: usize = 0;
-                    var num: i64 = int;
-                    while (num != 0) {
-                        digits += 1;
-                        num = @divFloor(num, 10);
-                    }
-                    i += (digits + 2);
-                },
-                .string => |str| {
-                    i += str.len + 1 + std.fmt.count("{}", .{str.len});
-                },
-                .list => i += 1,
-            }
+            i += try res.len();
         }
-
         return .{ .list = decodedList };
     } else {
         try stdout.print("Only Strings, Integers and Lists are available at this moment\n", .{});
