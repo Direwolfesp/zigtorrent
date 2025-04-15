561fd70c4a6e3cfa9b7c7075face9bb658e135f3
Direwolfesp <alvarolg365@gmail.com>
2025-04-15 22:24:12 +0200

    list parsing works, however i think the printing is bad

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,49 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const allocator = std.heap.page_allocator;
+const Map = std.StringArrayHashMapUnmanaged(Value);
 
 const ParseError = error{
     InvalidArgument,
 };
 
+// For sorting the key strings of the hash table
+const Ctx = struct {
+    map: Map,
+    pub fn lessThan(self: @This(), a: usize, b: usize) bool {
+        return std.mem.order(u8, self.map.keys()[a], self.map.keys()[b])
+            .compare(.lt);
+    }
+};
+
+const Tokens = enum(u8) {
+    List = 'l',
+    Dict = 'd',
+    Integer = 'i',
+    End = 'e',
+
+    fn isValid(token: u8) bool {
+        _ = std.meta.intToEnum(@This(), token) catch {
+            return std.ascii.isDigit(token);
+        };
+        return true;
+    }
+};
+
+test "token is valid" {
+    try std.testing.expect(Tokens.isValid('f') == false);
+    try std.testing.expect(Tokens.isValid('i') == true);
+    try std.testing.expect(Tokens.isValid('e') == true);
+    try std.testing.expect(Tokens.isValid('d') == true);
+    try std.testing.expect(Tokens.isValid('z') == false);
+    try std.testing.expect(Tokens.isValid('2') == true);
+}
+
 const Value = union(enum) {
     string: []const u8,
     integer: i64,
     list: ?std.ArrayList(Value),
+    dict: ?Map,
 
     pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype, nested: bool) !void {
         switch (self) {
@@ -32,13 +66,42 @@ const Value = union(enum) {
                     if (!nested) try writer.print("\n", .{});
                 }
             },
+            .dict => |dict_opt| {
+                if (dict_opt) |dict| {
+                    try writer.print("{{", .{});
+                    var iter = dict.iterator();
+                    while (iter.next()) |entry| {
+                        const key = Value{ .string = entry.key_ptr.* };
+                        const val = entry.value_ptr.*;
+                        try key.format(fmt, options, writer, true);
+                        try writer.print(":", .{});
+                        try val.format(fmt, options, writer, true);
+                    }
+                    try writer.print("}}", .{});
+                    if (!nested) try writer.print("\n", .{});
+                }
+            },
         }
     }
 
     pub fn deinit(self: *@This()) void {
         switch (self.*) {
             .list => |list_opt| {
-                if (list_opt) |list| list.deinit();
+                if (list_opt) |list| {
+                    for (list.items) |*item| {
+                        item.deinit();
+                    }
+                    list.deinit();
+                }
+            },
+            .dict => |dict_opt| {
+                if (dict_opt) |*dict| {
+                    for (dict.values()) |*val| {
+                        val.deinit();
+                    }
+                    var tmp = dict.*;
+                    tmp.deinit(allocator);
+                }
             },
             else => {},
         }
@@ -69,6 +132,17 @@ const Value = union(enum) {
                     }
                 }
                 return list_len;
+            },
+            .dict => |optDict| {
+                var dict_len: usize = 2;
+                if (optDict) |dict| {
+                    var iter = dict.iterator();
+                    while (iter.next()) |entry| {
+                        const key = Value{ .string = entry.key_ptr.* };
+                        dict_len += try key.len() + try entry.value_ptr.*.len();
+                    }
+                }
+                return dict_len;
             },
         };
     }
@@ -105,14 +179,14 @@ fn print(val: Value, writer: anytype, nested: bool) !void {
 
 fn decodeBencode(encodedValue: []const u8) !Value {
     // Strings
-    if (encodedValue[0] >= '0' and encodedValue[0] <= '9') {
+    if (std.ascii.isDigit(encodedValue[0])) {
         if (std.mem.indexOf(u8, encodedValue, ":")) |firstColon| {
             const strlen: u32 = try std.fmt.parseInt(u32, encodedValue[0..firstColon], 10);
             return .{ .string = encodedValue[firstColon + 1 .. (firstColon + 1 + strlen)] };
         } else return ParseError.InvalidArgument;
     }
     // Integers
-    else if (encodedValue[0] == 'i') {
+    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.Integer))) {
         const endIndex = std.mem.indexOf(u8, encodedValue, "e") orelse return ParseError.InvalidArgument;
         if ((endIndex - 1) > 0 and encodedValue[1] != '0') {
             return .{ .integer = try std.fmt.parseInt(i64, encodedValue[1..endIndex], 10) };
@@ -121,25 +195,43 @@ fn decodeBencode(encodedValue: []const u8) !Value {
         }
     }
     // Lists
-    else if (encodedValue[0] == 'l') {
+    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.List))) {
         var decodedList = std.ArrayList(Value).init(allocator);
         errdefer decodedList.deinit();
 
         var i: usize = 1; // i points to the beginning of a Bencode Value
-        while (i < encodedValue.len and encodedValue[i] != 'e') {
+        while (i < encodedValue.len and encodedValue[i] != @as(u8, @intFromEnum(Tokens.End))) {
             const res = try decodeBencode(encodedValue[i..]);
             try decodedList.append(res);
             i += try res.len();
 
-            if (i < encodedValue.len and encodedValue[i] != 'e' and encodedValue[i] != 'i' and
-                !(encodedValue[i] >= '0' and encodedValue[i] <= '9') and encodedValue[i] != 'l')
-            {
+            if (i < encodedValue.len and !Tokens.isValid(encodedValue[i])) {
                 return ParseError.InvalidArgument;
             }
         }
         return .{ .list = decodedList };
+    }
+    // Dict
+    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.Dict))) {
+        var decodedDict: Map = .empty;
+        errdefer decodedDict.deinit(allocator);
+
+        var i: usize = 1;
+        while (i < encodedValue.len and encodedValue[i] != @as(u8, @intFromEnum(Tokens.End))) {
+            const key = try decodeBencode(encodedValue[i..]);
+            i += try key.len();
+            const val = try decodeBencode(encodedValue[i..]);
+            try decodedDict.put(allocator, key.string, val);
+            i += try val.len();
+
+            if (i < encodedValue.len and !Tokens.isValid(encodedValue[i])) {
+                return ParseError.InvalidArgument;
+            }
+        }
+        decodedDict.sort(Ctx{ .map = decodedDict });
+        return .{ .dict = decodedDict };
     } else {
-        try stdout.print("Only Strings, Integers and Lists are available at this moment\n", .{});
+        try stdout.print("Only Strings, Integers, Lists and Dictionaries are available at the moment: {c}\n", .{encodedValue[0]});
         std.process.exit(1);
     }
 }
