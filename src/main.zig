ec402444685d6eb34d42fa2dd078e4b4828d807c
Direwolfesp <alvarolg365@gmail.com>
2025-04-17 19:11:26 +0200

    lol

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,9 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
-const Map = std.StringArrayHashMapUnmanaged(Value);
+const Map = std.StringArrayHashMap(Value);
 const File = std.fs.File;
+const Sha1 = std.crypto.hash.Sha1;
 
 const ParseError = error{
     InvalidArgument,
@@ -18,94 +19,58 @@ const Ctx = struct {
     }
 };
 
-const Tokens = enum(u8) {
-    List = 'l',
-    Dict = 'd',
-    Integer = 'i',
-    End = 'e',
-
-    fn isValid(token: u8) bool {
-        _ = std.meta.intToEnum(@This(), token) catch {
-            return std.ascii.isDigit(token);
-        };
-        return true;
-    }
-};
-
-test "token is valid" {
-    try std.testing.expect(Tokens.isValid('f') == false);
-    try std.testing.expect(Tokens.isValid('i') == true);
-    try std.testing.expect(Tokens.isValid('e') == true);
-    try std.testing.expect(Tokens.isValid('d') == true);
-    try std.testing.expect(Tokens.isValid('z') == false);
-    try std.testing.expect(Tokens.isValid('2') == true);
-}
-
 const Value = union(enum) {
     string: []const u8,
     integer: i64,
-    list: ?std.ArrayList(Value),
-    dict: ?Map,
+    list: std.ArrayList(Value),
+    dict: Map,
 
+    // Givan a Value -> JSON string
     pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype, nested: bool) !void {
         switch (self) {
-            .string => |str| {
-                try writer.print("\"{s}\"", .{str});
+            .string => |str| try std.json.stringify(str, .{}, writer),
+            .integer => |int| try std.json.stringify(int, .{}, writer),
+            .list => |list| {
+                try writer.print("[", .{});
+                for (list.items, 0..) |elem, i| {
+                    try elem.format(fmt, options, writer, true);
+                    if (i < list.items.len - 1) try writer.print(",", .{});
+                }
+                try writer.print("]", .{});
                 if (!nested) try writer.print("\n", .{});
             },
-            .integer => |int| {
-                try writer.print("{}", .{int});
+            .dict => |dict| {
+                try writer.print("{{", .{});
+                var iter = dict.iterator();
+                var i: usize = 0;
+                while (iter.next()) |entry| : (i += 1) {
+                    const key = Value{ .string = entry.key_ptr.* };
+                    const val = entry.value_ptr.*;
+                    try key.format(fmt, options, writer, true);
+                    try writer.print(":", .{});
+                    try val.format(fmt, options, writer, true);
+                    if (i < dict.count() - 1) try writer.print(",", .{});
+                }
+                try writer.print("}}", .{});
                 if (!nested) try writer.print("\n", .{});
-            },
-            .list => |list_opt| {
-                if (list_opt) |list| {
-                    try writer.print("[", .{});
-                    for (list.items, 0..) |elem, i| {
-                        try elem.format(fmt, options, writer, true);
-                        if (i < list.items.len - 1) try writer.print(",", .{});
-                    }
-                    try writer.print("]", .{});
-                    if (!nested) try writer.print("\n", .{});
-                }
-            },
-            .dict => |dict_opt| {
-                if (dict_opt) |dict| {
-                    try writer.print("{{", .{});
-                    var iter = dict.iterator();
-                    var i: usize = 0;
-                    while (iter.next()) |entry| : (i += 1) {
-                        const key = Value{ .string = entry.key_ptr.* };
-                        const val = entry.value_ptr.*;
-                        try key.format(fmt, options, writer, true);
-                        try writer.print(":", .{});
-                        try val.format(fmt, options, writer, true);
-                        if (i < dict.count() - 1) try writer.print(",", .{});
-                    }
-                    try writer.print("}}", .{});
-                    if (!nested) try writer.print("\n", .{});
-                }
             },
         }
     }
 
     pub fn deinit(self: *@This()) void {
         switch (self.*) {
-            .list => |list_opt| {
-                if (list_opt) |list| {
-                    for (list.items) |*item| {
-                        item.deinit();
-                    }
-                    list.deinit();
+            .list => |list| {
+                for (list.items) |*item| {
+                    item.deinit();
                 }
+                list.deinit();
             },
-            .dict => |dict_opt| {
-                if (dict_opt) |*dict| {
-                    for (dict.values()) |*val| {
-                        val.deinit();
-                    }
-                    var tmp = dict.*;
-                    tmp.deinit(allocator);
+            .dict => |dict| {
+                for (dict.values()) |*val| {
+                    val.deinit();
                 }
+                var tmp = dict;
+                tmp.deinit();
             },
             else => {},
         }
@@ -125,31 +90,30 @@ const Value = union(enum) {
                 const symbols: u8 = if (int >= 0) 2 else 3;
                 return digits + symbols;
             },
-            .string => |str| {
-                return str.len + 1 + std.fmt.count("{}", .{str.len});
-            },
-            .list => |optList| {
+            .string => |str| 1 + str.len + std.fmt.count("{}", .{str.len}),
+            .list => |list| {
                 var list_len: usize = 2;
-                if (optList) |list| {
-                    for (list.items) |elem| {
-                        list_len += try elem.len();
-                    }
+                for (list.items) |elem| {
+                    list_len += try elem.len();
                 }
                 return list_len;
             },
-            .dict => |optDict| {
+            .dict => |dict| {
                 var dict_len: usize = 2;
-                if (optDict) |dict| {
-                    var iter = dict.iterator();
-                    while (iter.next()) |entry| {
-                        const key = Value{ .string = entry.key_ptr.* };
-                        dict_len += try key.len() + try entry.value_ptr.*.len();
-                    }
+                var iter = dict.iterator();
+                while (iter.next()) |entry| {
+                    const key = Value{ .string = entry.key_ptr.* };
+                    dict_len += try key.len() + try entry.value_ptr.len();
                 }
                 return dict_len;
             },
         };
     }
+};
+
+const Commands = enum {
+    decode,
+    info,
 };
 
 pub fn main() !void {
@@ -161,43 +125,49 @@ pub fn main() !void {
         std.process.exit(1);
     }
 
-    const command = args[1];
+    const command = std.meta.stringToEnum(Commands, args[1]) orelse return;
 
-    if (std.mem.eql(u8, command, "decode")) {
-        const encodedStr = args[2];
-        var decodedStr = decodeBencode(encodedStr) catch {
-            try stdout.print("Invalid encoded value\n", .{});
-            std.process.exit(1);
-        };
-        defer decodedStr.deinit();
-        try print(decodedStr, stdout, false);
-    } else if (std.mem.eql(u8, command, "info")) {
-        const filename = args[2];
-        var file: File = std.fs.cwd().openFile(filename, .{}) catch |err| {
-            try stdout.print("Could not open file: {s}\nError: {!}", .{ filename, err });
-            std.process.exit(1);
-        };
-        defer file.close();
+    switch (command) {
+        .decode => {
+            const encodedStr = args[2];
+            var decodedStr = decodeBencode(encodedStr) catch {
+                try stdout.print("Invalid encoded value\n", .{});
+                std.process.exit(1);
+            };
+            defer decodedStr.deinit();
+            try print(decodedStr, stdout, false);
+        },
+        .info => {
+            const filename = args[2];
+            var file: File = try std.fs.cwd().openFile(filename, .{});
+            defer file.close();
 
-        var buffered = std.io.bufferedReader(file.reader());
-        var bufreader = buffered.reader();
-        var buffer = [_]u8{0} ** 1000;
-        _ = try bufreader.readUntilDelimiterOrEof(buffer[0..], '\n');
+            const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+            defer allocator.free(content);
 
-        var metaInfo: Value = try decodeBencode(buffer[0..]);
-        defer metaInfo.deinit();
+            var meta: Value = try decodeBencode(content);
+            defer meta.deinit();
 
-        const a = metaInfo.dict;
-        if (a) |infoDict| {
-            const announce: []const u8 = infoDict.get("announce").?.string;
-            try stdout.print("Tracker URL: {s}\n", .{announce});
+            const metaDict = if (meta == .dict) meta.dict else @panic("Invalid torrent file.\n");
+            const announce: Value = metaDict.get("announce").?;
+            try stdout.print("Tracker URL: {s}\n", .{announce.string});
 
-            const length: i64 = infoDict.get("info").?
-                .dict.?
+            const info: Value = metaDict.get("info") orelse @panic("info field not found\n");
+            const length: i64 = info.dict
                 .get("length").?
                 .integer;
             try stdout.print("Length: {d}\n", .{length});
-        }
+
+            var string = std.ArrayList(u8).init(allocator);
+            defer string.deinit();
+            try encodeBencodeValue(&string, info);
+
+            var sha1 = Sha1.init(.{});
+            sha1.update(string.items);
+            const hash_bytes: [Sha1.digest_length]u8 = sha1.finalResult();
+            const hash_hex = std.fmt.fmtSliceHexLower(&hash_bytes);
+            try stdout.print("Info Hash: {s}\n", .{hash_hex});
+        },
     }
 }
 
@@ -206,61 +176,85 @@ fn print(val: Value, writer: anytype, nested: bool) !void {
     try val.format("", .{}, writer, nested);
 }
 
+// Given a Bencoded string -> Value
 fn decodeBencode(encodedValue: []const u8) !Value {
-    // Strings
-    if (std.ascii.isDigit(encodedValue[0])) {
-        if (std.mem.indexOf(u8, encodedValue, ":")) |firstColon| {
-            const strlen: u32 = try std.fmt.parseInt(u32, encodedValue[0..firstColon], 10);
-            return .{ .string = encodedValue[firstColon + 1 .. (firstColon + 1 + strlen)] };
-        } else return ParseError.InvalidArgument;
-    }
-    // Integers
-    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.Integer))) {
-        const endIndex = std.mem.indexOf(u8, encodedValue, "e") orelse return ParseError.InvalidArgument;
-        if ((endIndex - 1) > 0 and encodedValue[1] != '0') {
-            return .{ .integer = try std.fmt.parseInt(i64, encodedValue[1..endIndex], 10) };
-        } else {
-            return ParseError.InvalidArgument;
-        }
-    }
-    // Lists
-    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.List))) {
-        var decodedList = std.ArrayList(Value).init(allocator);
-        errdefer decodedList.deinit();
-
-        var i: usize = 1; // i points to the beginning of a Bencode Value
-        while (i < encodedValue.len and encodedValue[i] != @as(u8, @intFromEnum(Tokens.End))) {
-            const res = try decodeBencode(encodedValue[i..]);
-            try decodedList.append(res);
-            i += try res.len();
-
-            if (i < encodedValue.len and !Tokens.isValid(encodedValue[i])) {
+    switch (encodedValue[0]) {
+        '0'...'9' => {
+            if (std.mem.indexOf(u8, encodedValue, ":")) |firstColon| {
+                const strlen: u32 = try std.fmt.parseInt(u32, encodedValue[0..firstColon], 10);
+                return .{ .string = encodedValue[firstColon + 1 .. (firstColon + 1 + strlen)] };
+            } else return ParseError.InvalidArgument;
+        },
+        'i' => {
+            const endIndex = std.mem.indexOf(u8, encodedValue, "e") orelse return ParseError.InvalidArgument;
+            if ((endIndex - 1) > 0 and encodedValue[1] != '0') {
+                return .{ .integer = try std.fmt.parseInt(i64, encodedValue[1..endIndex], 10) };
+            } else {
                 return ParseError.InvalidArgument;
             }
-        }
-        return .{ .list = decodedList };
-    }
-    // Dict
-    else if (encodedValue[0] == @as(u8, @intFromEnum(Tokens.Dict))) {
-        var decodedDict: Map = .empty;
-        errdefer decodedDict.deinit(allocator);
+        },
+        'l' => {
+            var decodedList = std.ArrayList(Value).init(allocator);
+            errdefer decodedList.deinit();
 
-        var i: usize = 1;
-        while (i < encodedValue.len and encodedValue[i] != @as(u8, @intFromEnum(Tokens.End))) {
-            const key = try decodeBencode(encodedValue[i..]);
-            i += try key.len();
-            const val = try decodeBencode(encodedValue[i..]);
-            try decodedDict.put(allocator, key.string, val);
-            i += try val.len();
-
-            if (i < encodedValue.len and !Tokens.isValid(encodedValue[i])) {
-                return ParseError.InvalidArgument;
+            var i: usize = 1; // i points to the beginning of a Bencode Value
+            while (i < encodedValue.len and encodedValue[i] != 'e') {
+                const res = try decodeBencode(encodedValue[i..]);
+                try decodedList.append(res);
+                i += try res.len();
             }
-        }
-        decodedDict.sort(Ctx{ .map = decodedDict });
-        return .{ .dict = decodedDict };
-    } else {
-        try stdout.print("Only Strings, Integers, Lists and Dictionaries are available at the moment: {c}\n", .{encodedValue[0]});
-        std.process.exit(1);
+            return .{ .list = decodedList };
+        },
+        'd' => {
+            var decodedDict: Map = Map.init(allocator);
+            errdefer decodedDict.deinit();
+
+            var i: usize = 1;
+            while (i < encodedValue.len and encodedValue[i] != 'e') {
+                const key = try decodeBencode(encodedValue[i..]);
+                i += try key.len();
+                const val = try decodeBencode(encodedValue[i..]);
+                try decodedDict.put(key.string, val);
+                i += try val.len();
+            }
+            decodedDict.sort(Ctx{ .map = decodedDict });
+            return .{ .dict = decodedDict };
+        },
+        else => {
+            try stdout.print("Only Strings, Integers, Lists and Dictionaries are available at the moment: {c}\n", .{encodedValue[0]});
+            std.process.exit(1);
+        },
+    }
+}
+
+// Given a Value -> Bencoded string
+fn encodeBencodeValue(string: *std.ArrayList(u8), toEncode: Value) !void {
+    switch (toEncode) {
+        .string => |str| {
+            try std.json.stringify(str.len, .{}, string.writer());
+            try string.append(':');
+            try string.appendSlice(str);
+        },
+        .integer => |int| {
+            try string.append('i');
+            try std.json.stringify(int, .{}, string.writer());
+            try string.append('e');
+        },
+        .list => |list| {
+            try string.append('l');
+            for (list.items) |item| {
+                try encodeBencodeValue(string, item);
+            }
+            try string.append('e');
+        },
+        .dict => |dict| {
+            try string.append('d');
+            var it = dict.iterator();
+            while (it.next()) |kv| {
+                try encodeBencodeValue(string, Value{ .string = kv.key_ptr.* });
+                try encodeBencodeValue(string, kv.value_ptr.*);
+            }
+            try string.append('e');
+        },
     }
 }
