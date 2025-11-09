2e9a0102c8a4bf731b3a2d12988561fc4bad5cf0
Direwolfesp <alvarolg365@gmail.com>
2025-11-09 21:10:12 +0100

    peer: wip, separate in two functions

--- src/peer.zig
+++ src/peer.zig
@@ -5,8 +5,10 @@ pub const State = struct {
     connection: enum {
         /// initial state
         disconnected,
+        /// waiting for the peer to accept our connect()
+        connecting,
         /// tcp connection stablished
-        stablished,
+        connected,
         /// interchanging handshakes
         handshaking,
         /// registering peer bitfield/have
@@ -17,9 +19,9 @@ pub const State = struct {
 };
 
 pub const Connection = struct {
-    loop: *Epoll,
+    loop: ?*Epoll,
     addr: std.net.Address,
-    socket: std.posix.fd_t,
+    socket: std.posix.fd_t = -1,
     peer_bitfield: std.DynamicBitSetUnmanaged,
     state: State = .{},
 
@@ -37,62 +39,78 @@ pub const Connection = struct {
 
     const Self = @This();
 
-    pub fn connect(
+    pub fn init(
         alloc: std.mem.Allocator,
         addr: std.net.Address,
         num_pieces: usize,
-        loop: *Epoll,
         write_buf_len: usize,
         read_buf_len: usize,
     ) !Self {
-        const sock_flags = std.posix.SOCK.STREAM | std.posix.SOCK.NONBLOCK | std.posix.SOCK.CLOEXEC;
-        const sockfd = try std.posix.socket(addr.any.family, sock_flags, std.posix.IPPROTO.TCP);
-        try std.posix.connect(sockfd, &addr.any, addr.getOsSockLen());
-
-        var writer: Writer = try .init(alloc, write_buf_len, sockfd);
+        const writer: Writer = try .init(alloc, write_buf_len, -1); // placeholder fd
         errdefer writer.deinit(alloc);
 
-        var reader: Reader = try .init(alloc, read_buf_len);
+        const reader: Reader = try .init(alloc, read_buf_len);
         errdefer reader.deinit(alloc);
 
         const bitfield: std.DynamicBitSetUnmanaged = try .initEmpty(alloc, num_pieces);
 
-        var conn = Self{
-            .loop = loop,
+        return Self{
+            .loop = null,
             .addr = addr,
-            .socket = sockfd,
             .writer = writer,
             .reader = reader,
             .peer_bitfield = bitfield,
+            .state = .{},
+        };
+    }
+
+    pub fn connect(self: *Self, loop: *Epoll) !void {
+        const sock_flags = std.posix.SOCK.STREAM | std.posix.SOCK.NONBLOCK | std.posix.SOCK.CLOEXEC;
+        const sockfd = try std.posix.socket(self.addr.any.family, sock_flags, std.posix.IPPROTO.TCP);
+
+        // connect non-blocking
+        const res: ?void = std.posix.connect(
+            sockfd,
+            &self.addr.any,
+            self.addr.getOsSockLen(),
+        ) catch |err| switch (err) {
+            error.WouldBlock => null,
+            else => return err,
         };
 
-        // update state
-        conn.state.connection = .stablished;
-        // register this connection to the event loop
-        try conn.loop.newClient(&conn);
+        self.socket = sockfd;
+        self.loop = loop;
 
-        return conn;
+        if (res == null) {
+            self.state.connection = .connecting;
+            try loop.writeMode(self); // wait for EPOLLOUT
+        } else {
+            self.state.connection = .connected;
+            try loop.newClient(self); // register EPOLLIN
+        }
     }
 
     pub fn disconnect(self: *Self, alloc: std.mem.Allocator) !void {
         self.writer.deinit(alloc);
         self.peer_bitfield.deinit(alloc);
-        try self.loop.removeClient(self);
+        if (self.loop) |l| {
+            try l.removeClient(self);
+        }
         std.posix.close(self.socket);
     }
 
     pub fn handshake(self: *Self, peer_id: [20]u8, torrent: *const TorrentFile) !void {
         self.state.connection = .handshaking;
         if (!(try self.writer.writeHandshake(peer_id, torrent))) {
-            try self.loop.writeMode(self);
+            try self.loop.?.writeMode(self);
         } else {
             self.state.connection = .normal;
-            try self.loop.readMode(self);
+            try self.loop.?.readMode(self);
         }
     }
 };
 
-test "peer: handshake with peer" {
+test "peer: handshake with peer (init + connect)" {
     const alloc = std.testing.allocator;
 
     var torr = try TorrentFile.open(alloc, "src/tests/torrents/debian-12.11.0-amd64-netinst.iso.torrent");
@@ -100,7 +118,6 @@ test "peer: handshake with peer" {
 
     var tr: Tracker = try .init(&torr.meta);
     defer tr.deinit(alloc);
-
     try tr.announce(alloc);
 
     var picker: PiecePicker = try .init(torr.meta, alloc);
@@ -110,13 +127,22 @@ test "peer: handshake with peer" {
     defer loop.deinit();
 
     const addr = std.net.Address{ .in = tr.peers.?[0] };
-    var client: Connection = try .connect(alloc, addr, torr.meta.getNumPieces(), &loop, 1024, 0x4000);
+
+    var client: Connection = try Connection.init(
+        alloc,
+        addr,
+        torr.meta.getNumPieces(),
+        1024, // write buffer
+        0x4000, // read buffer
+    );
     defer client.disconnect(alloc) catch |err| {
-        log.err("Error while shuting down client '{any}'. Error: {t}", .{ client.addr, err });
+        log.err("Error shutting down client '{any}': {t}", .{ client.addr, err });
     };
 
+    try client.connect(&loop);
+
     client.handshake(tr.peer_id, &torr.meta) catch |err| {
-        log.err("Could not handshake with peer '{any}'. Error: {t}", .{ addr, err });
+        log.err("Could not handshake with peer '{any}': {t}", .{ addr, err });
     };
 
     try std.testing.expectEqual(client.state.connection, .normal);
